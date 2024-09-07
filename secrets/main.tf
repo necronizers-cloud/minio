8cead845144949bdd57e47f888814f1348ce71c1
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2024-09-07 20:36:08 +0530

    Secrets moved to OpenTofu

---
+++ secrets/main.tf
@@ -0,0 +1,90 @@
+// Passwords for MinIO Tenant Users
+resource "random_password" "root_password" {
+  length           = 16
+  lower            = true
+  numeric          = true
+  special          = true
+  override_special = "-_*/"
+  min_special      = 2
+}
+
+resource "random_password" "photoatom_password" {
+  length           = 16
+  lower            = true
+  numeric          = true
+  special          = true
+  override_special = "-_*/"
+  min_special      = 2
+}
+
+resource "random_password" "postgres_password" {
+  length           = 16
+  lower            = true
+  numeric          = true
+  special          = true
+  override_special = "-_*/"
+  min_special      = 2
+}
+
+// MinIO Storage Configuration
+resource "kubernetes_secret" "storage_configuration" {
+  metadata {
+    name      = var.storage_configuration_name
+    namespace = var.namespace
+
+    labels = {
+      app       = "minio"
+      component = "secret"
+    }
+  }
+
+  data = {
+    "config.env" = <<EOT
+export MINIO_ROOT_USER="minio"
+export MINIO_ROOT_PASSWORD="${random_password.root_password.result}"
+export MINIO_STORAGE_CLASS_STANDARD="EC:2"
+export MINIO_BROWSER="on"
+EOT
+  }
+
+  type = "Opaque"
+}
+
+// MinIO Users Configuration
+resource "kubernetes_secret" "photoatom_user" {
+  metadata {
+    name      = var.photoatom_user_name
+    namespace = var.namespace
+
+    labels = {
+      app       = "minio"
+      component = "secret"
+    }
+  }
+
+  data = {
+    CONSOLE_ACCESS_KEY = "photoatom"
+    CONSOLE_SECRET_KEY = "${random_password.photoatom_password.result}"
+  }
+
+  type = "Opaque"
+}
+
+resource "kubernetes_secret" "postgres_user" {
+  metadata {
+    name      = var.postgres_user_name
+    namespace = var.namespace
+
+    labels = {
+      app       = "minio"
+      component = "secret"
+    }
+  }
+
+  data = {
+    CONSOLE_ACCESS_KEY = "postgres"
+    CONSOLE_SECRET_KEY = "${random_password.postgres_password.result}"
+  }
+
+  type = "Opaque"
+}
