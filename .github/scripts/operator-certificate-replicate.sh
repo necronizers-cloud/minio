08ffa67a37f59a536786f3f8a1d0b5b112707fa5
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2024-10-13 09:40:47 +0530

    Script for cert replication

---
+++ .github/scripts/operator-certificate-replicate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+# Check if certs are required to be replicated
+CHECK_TENANT_CERT=$(kubectl get secrets -n minio-operator | grep -c "operator-ca-tls-photoatom-object-storage" || true)
+
+if [[ "$CHECK_TENANT_CERT" == "0" ]]
+then
+
+  # Sleep for some time to give time for certs creation
+  echo "Sleeping for 30 seconds to give time for certificates creation"
+  sleep 30
+
+  # Replicate certificates from MinIO Namespace to Operator Namespace
+  echo "Replicating certificates from MinIO Namespace to Operator Namespace if it does not exist"
+  kubectl get secrets -n minio minio-tls -o=jsonpath='{.data.ca\\.crt}' | base64 -d > ca.crt
+  kubectl create secret generic operator-ca-tls-photoatom-object-storage --from-file=ca.crt -n minio-operator
+
+  # Restart MinIO Operator Deployment
+  kubectl rollout restart deployments.apps/minio-operator -n minio-operator
+  kubectl rollout status deployments.apps/minio-operator -n minio-operator
+
+else
+  echo "Certificates already replicated, skipping ahead..."
+fi
